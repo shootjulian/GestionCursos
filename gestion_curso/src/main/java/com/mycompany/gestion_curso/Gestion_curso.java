fa356a9ea3b7edfc54fadc46596ebeb5d0485780
shootjulian <julian.gomez2@estudiantesunibague.edu.co>
2026-08-20 16:04:34 -0500

    Se creo el proyecto de Java Maven

---
+++ gestion_curso/src/main/java/com/mycompany/gestion_curso/Gestion_curso.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gestion_curso;
+
+/**
+ *
+ * @author Julim
+ */
+public class Gestion_curso {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
